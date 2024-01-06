1cdbf0f53125ac086dae79b03e1d417f55a6e4f2
bphkns <bphkns@gmail.com>
2024-01-06 22:13:46 +0530

    added autocomplete

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader=" "
-vim.api.nvim_exec([[
-  set number
-  set relativenumber
-]], true)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -20,29 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins={
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
+require("vim-options")
+require("lazy").setup("plugins")
 
-local opts={}
 
-require("lazy").setup(plugins, opts)
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = { "lua", "javascript", "typescript", "angular", "html", "sql", "bash" },
-  highlight = { enable = true },
-  indent = { enable = true }  
-})
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "angularls", "tsserver", "quick_lint_js", "html" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.angularls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.quick_lint_js.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
