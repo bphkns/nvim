fe0a3f0bce477254bf92e28fa19f28980d287aeb
Bikash Phukan <bikash@spotdraft.com>
2024-01-08 20:32:04 +0530

    wip

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,15 @@
+
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function ()    
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find cwd files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc= "live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = "Recent" })
     end
   },
   {
